3126d3f8d68845bb5fd45c48dea94d8655d9a378
Ryan Weaver <ryan@thatsquality.com>
2016-04-26 07:56:29 -0400

    Add Firewall+security

--- index.php
+++ index.php
@@ -4,6 +4,17 @@ require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->register(new Silex\Provider\SecurityServiceProvider());
+$app['security.firewalls'] = array(
+    'main' => array(
+        'pattern' => '^/',
+        'users' => array(
+            // raw password is foo
+            'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
+        ),
+    ),
+);
+
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name);
 });
